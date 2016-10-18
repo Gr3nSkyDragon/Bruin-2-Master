3ed4181cc05bfae85392c4b2b6e4099bd1c9b6ec
Anthony Cuenca <acuen753@students.bju.edu>
2016-10-18 16:24:40 -0400

    Create location.h

---
+++ location.h
@@ -0,0 +1,11 @@
+#IFNDEF LOCATION_H
+#define LOCATION_H
+
+#include <string> 
+#include <cmath> 
+#include <vector>
+#include <algorithm> 
+
+// Add stuff in here for the user's location and destination 
+
+#endif 
